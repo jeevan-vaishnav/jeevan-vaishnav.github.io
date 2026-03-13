d004457951bd37672c3447e0634fdb62ec440504
jeevan-vaishnav <jeevanvaishnavv@gmail.com>
2026-03-13 17:15:07 +0000

    update

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,10 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === "production";
-
 const nextConfig: NextConfig = {
   output: "export",
-  images: { unoptimized: true },
-  basePath: isProd ? "/jeevan-vaishnav.github.io" : "",
-  assetPrefix: isProd ? "/jeevan-vaishnav.github.io/" : ""
+  images: {
+    unoptimized: true
+  }
 };
 
 export default nextConfig;
