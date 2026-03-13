2eada8556df2e27a8b490d27f3c6c0aad48330d4
jeevan-vaishnav <jeevanvaishnavv@gmail.com>
2026-03-13 16:52:17 +0000

    update portfolio section

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,16 @@
 import type { NextConfig } from "next";
 
+const repo = "jeevan-vaishnav.github.io";
+
+
 const nextConfig: NextConfig = {
   output: "export",
   images: {
     unoptimized: true
-  }
+  },
+  basePath: `/${repo}`,
+  assetPrefix: `/${repo}/`
+
 };
 
 export default nextConfig;
