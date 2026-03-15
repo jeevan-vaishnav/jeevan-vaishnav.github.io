4773497ebb0beb9f4bbb0e5dc4054120dffc7c61
jeevan-vaishnav <jeevanvaishnavv@gmail.com>
2026-03-15 11:08:25 +0000

    pf

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  images: {
-    unoptimized: true
-  },
-  basePath: "",
-  assetPrefix: "",
-
 };
 
 export default nextConfig;
